8a649ba4416e25210a547acada24c943a13ccbb7
Andreas Haas <andy.haas.de@gmail.com>
2026-04-16 14:42:13 +0200

    tag04 refactoring #1

--- src/main/java/tag04/unterricht/code_XML_JSON/ClientInterface.java
+++ src/main/java/tag04/unterricht/code_XML_JSON/ClientInterface.java
@@ -3,8 +3,9 @@ package tag04.unterricht.code_XML_JSON;
 
 import tag04.unterricht.code_XML_JSON.externe_bibliothek.XmlDatei;
 
-// Das Client-Interface ist die Schnittstelle zwischen dem Client und der externen Bibliothek. Der Client kommuniziert mit der externen Bibliothek über den Adapter, welcher das Interface implementieren muss.
+// Das Client-Interface ist die Schnittstelle zwischen dem Client und der externen Bibliothek.
+// Der Client kommuniziert mit der externen Bibliothek über den Adapter, welcher das Interface implementieren muss.
 public interface ClientInterface {
 
-  public abstract XmlDatei getFahrzeitXml(XmlDatei xmlDaten);
+  XmlDatei getFahrzeitXml(XmlDatei xmlDaten);
 }
