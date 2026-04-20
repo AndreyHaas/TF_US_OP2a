009f50b3c38f40474dc3d123ef6c01a11a9a0339
Andreas Haas <andy.haas.de@gmail.com>
2026-04-20 08:10:50 +0200

    tag05 in main methode singleton getestet.

--- src/main/java/tag05/Main.java
+++ src/main/java/tag05/Main.java
@@ -6,6 +6,8 @@ public class Main {
         EinSingleton instance1 = EinSingleton.getInstance();
         EinSingleton instance2 = EinSingleton.getInstance();
 
-        System.out.printf("");
+        System.out.println("i0.equals(i1) is " + instance0.equals(instance1));
+        System.out.println("i1.equals(i2) is " + instance1.equals(instance2));
+        System.out.println("i2.equals(i0) is " + instance2.equals(instance0));
     }
 }
