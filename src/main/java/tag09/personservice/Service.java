90a8ad9392a3635c1e18e9fd8238bbe3655fa034
Andreas Haas <andy.haas.de@gmail.com>
2026-04-23 14:44:15 +0200

    tag09 JavaDoc überschrieben.

--- src/main/java/tag09/personservice/Service.java
+++ src/main/java/tag09/personservice/Service.java
@@ -5,11 +5,18 @@ import java.util.List;
 public interface Service {
 
   /**
-   * Sucht alle Personen mit dem angegebenen Namen.
+   * Findet alle Personen anhand ihres Namens.
+   * <p>
+   * Die Methode durchsucht die interne Liste aller Personen und gibt jede Person zur&uuml;ck,
+   * deren Name exakt mit dem angegebenen Suchnamen &uuml;bereinstimmt.
+   * </p>
    *
-   * @param name der Name der gesuchten Person (darf nicht null sein)
-   * @return eine Liste aller Personen, deren Name mit dem angegebenen Namen übereinstimmt; falls
-   * keine Person gefunden wird, wird eine leere Liste zurückgegeben
+   * @param name der Name der gesuchten Person (Gro&szlig;-/Kleinschreibung wird beachtet)
+   * @return eine {@link List} von {@link Person}-Objekten mit dem gesuchten Namen;
+   *         wenn keine Person gefunden wird, wird eine leere Liste zur&uuml;ckgegeben
+   *         (niemals null)
+   *
+   * @throws NullPointerException wenn der &uuml;bergebene Name null ist
    */
   List<Person> findPersonByName(String name);
 }
